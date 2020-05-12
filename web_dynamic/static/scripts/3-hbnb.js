36371ca4a7f4b0e6d04bc245ff8247283992a17e
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2020-05-12 12:16:09 +0000

    Added comments.  Changed HTML to display everything correctly.

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
     } else {
       delete amenities[($(this).data().id)];
     }
-    // Displays amenities checked in amenities fileter box
+    // Displays amenities checked in amenities filter
     let text = '';
     let count = 0;
     $.each(amenities, (id, name) => {
@@ -23,6 +23,7 @@ $( document ).ready(function() {
       $('div.amenities h4').text(text);
     }
   });
+  // Fetch status of API and change of circle, red for available, grey for not
   const url = 'http://localhost:5001/api/v1/status/';
   $.get(url, (data) => {
     if (data.status === "OK") {
@@ -31,6 +32,7 @@ $( document ).ready(function() {
       $('#api_status').removeClass('available');
     }
   });
+  // Fetch data about places from API and insert them into HTML
   $.ajax({
     type: 'POST',
     contentType: 'application/json',
@@ -38,15 +40,20 @@ $( document ).ready(function() {
     url: 'http://0.0.0.0:5001/api/v1/places_search',
     dataType: 'json',
     success: function (data) {
-      let place = data[i];
       for (let i = 0; i < data.length; i++) {
-	data_list = [ $('#places').append(
-	  '<article><div class="title"><h2>' + place.name + '<h2>' +
-	  '<div class="price_by_night">' + place.price_by_night + '</div>' +
-          '<div class="information">' +
-          '<div class="max_guest"><br />' + place.max_guest + '</div>' +
-          '<div class="number_rooms">' + place.number_rooms + '</div>' +
-          '<div class="number_bathrooms">' + place.number_bathrooms + '</div>' +
+        let place = data[i];
+        data_list = [ $('section.places').append(
+          '<article><div class="title"><h2>' + place.name + '</h2>' +
+          '<div class="price_by_night">' + place.price_by_night + '</div>' +
+          '</div>' + '<div class="information">' + '<div class="max_guest">' +
+          '<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
+          '<br />' + place.max_guest + ' Guests</div>' +
+          '<div class="number_rooms">' +
+          '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
+          '<br />' + place.number_rooms + ' Bedrooms</div>' +
+          '<div class="number_bathrooms">' +
+          '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
+          '<br />' + place.number_bathrooms + ' Bathroom</div>' +
           '</div>' +
           '<div class=description">' + place.description + '</div>' +
           '</article>')];
